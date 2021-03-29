54e3fed54cb6bf3acd0621b7d91293094dc9100a
Robert Maxton <robert.maxton42@gmail.com>
2021-03-29 15:09:49 -0700

    PHOAS?

---
+++ Diagrammatic/PHOAS.fs
@@ -0,0 +1,20 @@
+namespace Diagrammatic
+
+module PHOGraph
+
+  type Graph <'a, 'v> =
+    private
+    |
+
+
+  // type PLambda<'a> =
+  //   private
+  //   | Var 'a
+  //   | Int int
+  //   | Bool bool
+  //   | If (PLambda 'a) (PLambda 'a) (PLambda 'a)
+  //   | Add (PLambda 'a) (PLambda 'a)
+  //   | Mult (PLambda 'a) (PLambda 'a)
+  //   | Eq (PLambda 'a) (PLambda 'a)
+  //   | Lam ('a -> PLambda 'a)
+  //   | Mu1 ('a -> PLambda 'a)

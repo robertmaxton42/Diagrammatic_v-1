0235fa8b8ad55f9c89f8c8dd6c8e2e079b6eaa13
Robert Maxton <robert.maxton42@gmail.com>
2021-04-04 01:44:17 -0700

    PHOAS v0

--- Diagrammatic/PHOAS.fs
+++ Diagrammatic/PHOAS.fs
@@ -1,11 +1,55 @@
 namespace Diagrammatic
 open Higher
 
-module PHOGraph
+module PHOGraph =
 
-  type Graph <'a, 'v> =
+  let fix f = 
+    let rec fix' f = lazy f (fix' f) 
+    (fix' f).Value
+
+  type Abstract = class end
+  type GraphRec<'F, 'a> = 
     private
-    |
+    | Var of 'a
+    | Mu of ('a seq -> App<'F, GraphRec<'F, 'a>> seq)
+    | In of App<'F, GraphRec<'F, 'a>>
+
+  type Graph<'F> = private Graph of GraphRec<'F, Abstract>
+  
+  module Graph =
+    let hide gr = Graph gr
+    let reveal (Graph g) = g
+
+    let private force (x: Lazy<'a>) = x.Value
+
+    let rec private fixVal (v: 'a when 'a : equality) f = 
+      let v' = f (Lazy.CreateFromValue v)
+      if v = v' then v else fixVal v' f
+
+
+    let gfold (link: 't -> 'c) (fixreduce: (Lazy<'t seq> -> 'c seq) -> 'c) (ev: #Functor<'F>) (reduce: App<'F, 'c> -> 'c) = 
+      let rec trans graph = 
+        match graph with
+        | Var x -> link x
+        // The argument to fixmap should be able to take an infinite series of constants
+        // and take only as many as it needs (only as many as there are actual branches
+        // from the current node)
+        | Mu cycle -> fixreduce (Seq.map (reduce << ev.Map trans) << cycle << force)
+        | In subg -> reduce (ev.Map trans subg)
+      trans << reveal 
+
+    let fold (ev: #Functor<'F>) (alg: App<'F, 'c> -> 'c) (k: 'c) =
+      gfold id (fun g -> (Seq.head << g) (lazy Seq.initInfinite (fun _ -> k))) ev alg
+
+    let cfold (ev: #Functor<'F>) (reduce: App<'F, 't> -> 't)=
+      gfold id (Seq.head << fix) ev reduce
+
+    let sfold (ev: #Functor<'F>) (alg: App<'F, 't> -> 't) (k: 't) =
+      gfold id (Seq.head << (fixVal (Seq.initInfinite (fun _ -> k)))) ev alg
+
+  // type Graph <'a, 'v> =
+  //   private
+  //   | 
 
 
   // type PLambda<'a> =
